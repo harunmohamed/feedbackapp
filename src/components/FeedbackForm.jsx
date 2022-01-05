a0e36d744b69638033171d8fcaa4001a37858606
harunmohamed <harunmohamed901@gmail.com>
2022-01-05 16:57:06 +0200

    form validation and fadein animation

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,18 +1,46 @@
 import { useState } from 'react'
+import RatingSelect from './RatingSelect'
 import Card from "./shared/Card"
 import Button from './shared/Button'
 
-function FeedbackForm() {
+function FeedbackForm({handleAdd}) {
     const  [text, setText] = useState('')
+    const  [rating, setRating] = useState(10)
+    const  [btnDisabled, setbtnDisabled] = useState(true)
+    const  [message, setMessage] = useState('')
+
     const handleTextChange = (e) => {
+        if(text === ''){
+            setbtnDisabled(true)
+            setMessage(null)
+        } else if (text !== '' && text.trim().length <= 10){
+            setbtnDisabled(true)
+            setMessage('Text must be at least 10 characters')
+        } else {
+            setbtnDisabled(false)
+            setMessage(null)
+        }
+
         setText(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if(text.trim().length > 10){
+            const newFeedback = {
+                text,
+                rating,
+            }
+            handleAdd(newFeedback)
+            setText('')
+        }
     }
 
     return (
         <Card>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h2>How would you rate your service with us?</h2>
-                {/* @todo: rating select component */}
+                <RatingSelect select={(rating) => setRating(rating)}  />
                 <div className="input-group">
                     <input 
                         onChange={handleTextChange} 
@@ -20,8 +48,10 @@ function FeedbackForm() {
                         placeholder="Write a review" 
                         value={text}
                     />
-                    <Button type='submit' />
+                    <Button type='submit' isDisabled={btnDisabled}>Send</Button>
                 </div>
+
+                {message && <div className='message'>{message}</div>}
             </form>
         </Card>
     )
